4fc4f9221d02878689eb5892bda7c51ea7c752a7
barneyb <bboisvert@gmail.com>
2019-10-21 14:36:05 -0700

    also print send stats after the queue stats

--- stats.sh
+++ stats.sh
@@ -3,3 +3,22 @@ set -e
 
 cd `dirname $0`
 wc -l queue/*.txt
+
+CHANNELS=`cat log.txt | cut -d ] -f 2 | cut -d ' ' -f 2 | sort -u`
+TYPES=`cat log.txt | cut -d ] -f 2 | cut -d ' ' -f 3 | sort -u`
+
+for c in $CHANNELS; do
+  echo $c
+  n=`cat log.txt | grep "\] $c " | wc -l`
+  if [ $n -eq 0 ]; then
+    continue
+  fi
+  echo " total: $n"
+  for t in $TYPES; do
+    n=`cat log.txt | grep "\] $c $t " | wc -l`
+    if [ $n -eq 0 ]; then
+      continue
+    fi
+    echo "  $t: $n"
+  done
+done
