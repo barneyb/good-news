5efea97e647005435d7707624dcda12338a85458
barneyb <bboisvert@gmail.com>
2019-10-21 14:24:12 -0700

    simple 'add' script for adding stuff to a queue

---
+++ add.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+cd `dirname $0`
+cd queue
+
+declare -a QUEUES
+
+echo "Select a queue from below (or exit and touch a new one)"
+i=0
+for fn in `ls *.txt`; do
+  i=$((i+1))
+  QUEUES[$i]=$fn
+  echo "  $i) $fn (`wc -l $fn | cut -d ' ' -f 1`)"
+done
+
+q="glerg"
+until [ "$q" -gt 1 -a "$q" -lt $i ] 2>/dev/null; do
+  echo -n "Queue (1-$i, 'q' to quit): "
+  read q
+  if [ "$q" = "q" ]; then
+    exit 0
+  fi
+done
+
+echo
+echo "Adding to '${QUEUES[$q]}'; hit Ctrl-D when done..."
+cp ${QUEUES[$q]} ${QUEUES[$q]}.bak
+cat >> ${QUEUES[$q]}
+echo Done!
+wc -l *.txt
+
